d5782c8d68c43b2d92568dc8e83ac9a9d46231ea
DesperatE2002 <desperatE2002@github.com>
2026-01-08 13:47:02 +0300

    fix: ClerkProvider eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,16 +32,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
+}: Readonly<{\n  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
